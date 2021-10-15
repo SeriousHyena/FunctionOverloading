d5769b9794d021d7bfdd62c913b81476a720531c
SeriousHyena <emackiii@gmail.com>
2021-10-15 12:04:54 -0700

    added function to ge the cube of a number

--- main.cpp
+++ main.cpp
@@ -6,13 +6,16 @@ using namespace std;
 //declare functions here
 int getResult(int num1, int num2);
 string getResult(string str1, string str2);
+int getResult(int cubeBase);
 
 int main()
 {
+	int cubeResult = getResult(5);							//invoke the function from main
 	int resultNum = getResult(30, 20);
 	string nameResult = getResult("Ernie", "Mack");
 	cout << "Result num is: " << resultNum << endl;
 	cout << "Name result is: " << nameResult << endl;
+	cout << "The cube of cubBase is: " << cubeResult << endl;
 
 	return 0;
 }
@@ -25,4 +28,9 @@ int getResult(int num1, int num2)
 string getResult(string str1, string str2)
 {
 	return str1 + " " + str2;
+}
+
+int getResult(int cubeBase)
+{
+	return cubeBase * cubeBase * cubeBase;
 }
